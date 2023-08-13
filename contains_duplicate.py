f305f4205f04aa4e02a304643790ba162a9cfad6
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-08-13 03:40:17 -0400

    Contains_duplicate_o(n)

---
+++ contains_duplicate.py
@@ -0,0 +1,14 @@
+
+def containsDuplicate(self, nums):
+
+    seen = set()
+    for number in nums:
+        if number in seen:
+            return True
+        seen.add(number)
+    return False
+
+# in this code the time complexity of this algorithm is o(n)
+# for number in nums it  would be 0(n), since it iterates through all the array
+# if number in nums would be o(1), since we are using set which stores a unique number
+# if it finds the  number it gives true, else adds the number 

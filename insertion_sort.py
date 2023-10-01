b0ca797ebe28630b14b2caf2a5cab908c59806e0
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-09-30 20:39:50 -0400

    Insertion Sort O(n^2)

---
+++ insertion_sort.py
@@ -0,0 +1,20 @@
+def insertion_sort(arr):
+    # Start from the second element since the first element is already considered sorted.
+    for i in range(1, len(arr)):
+        # Store the current element to be inserted into the sorted portion.
+        current_element = arr[i]
+        j = i - 1
+
+        # Move elements of the sorted portion that are greater than the current element
+        # one position to the right.
+        while j >= 0 and current_element < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        # Insert the current element into its correct position.
+        arr[j + 1] = current_element
+
+# Example usage:
+my_list = [12, 11, 13, 5, 6]
+insertion_sort(my_list)
+print("Sorted array is:", my_list)

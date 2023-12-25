5b4ce6301d47011b0ee3ad41aed096efcecc95b2
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2023-12-24 22:53:53 -0500

    is Palindrome

--- Arrays & Hashing/long_consecutive.py
+++ Arrays & Hashing/long_consecutive.py
@@ -4,33 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        preFix = []
-        postFix = [1] * len(nums)
-        output = [1] * len(nums)
-        sum = 1
-        for index in range(len(nums)):
-            if index == 0:
-                preFix.append(nums[index])
-            else:
-                preFix.append(preFix[index-1] * nums[index])
-
-        for index in range(len(nums)-1, -1, -1):
-            if index == len(nums) - 1:
-                postFix[index] = nums[index]
-            else:
-                postFix[index] = postFix[index+1] * nums[index]
         
-        for index in range(len(nums)):
-            counter = index - 1
-            counter1 = index + 1
-            if index == 0:
-                output[index] = 1 * postFix[counter1]
-            elif index == len(nums)-1:
-                 output[index] = preFix[counter] * 1
-            else:
-                output[index] = preFix[counter] * postFix[counter1]
-
-        return output, preFix, postFix
 
 
 

a31872405904c62faa1a2024114099f3e81b7a16
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2023-12-27 22:48:26 -0500

    Folders Fix

--- previous/isPalindrome.py
+++ previous/isPalindrome.py
@@ -1,21 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
-    def isPalindrome(self, head):
+    def isPalindrome(self, s):
         """
-        :type head: ListNode
+        :type s: str
         :rtype: bool
         """
-        array = []
-        pointer = head
-        while pointer != None:
-            array += pointer.val,
-            pointer = pointer.next
-        return array == array[::-1]
-        
 
+        s = [char.lower() for char in s if char.isalnum()]
+        return s == s[::-1]
+    
 
-        
+    #time comopleixty of the algorithm will be 0(n), since all step
+"""
+Iterating over the characters in the string s and applying char.lower() and char.isalnum() operations takes O(N) time complexity, where N is the length of the original string s.
+
+Creating a list comprehension based on the filtered characters also takes O(N) time complexity because each character is processed once.
+
+Reversing a list using slicing (s[::-1]) takes O(N) time complexity because it needs to create a reversed copy of the list.
+
+Finally, comparing two lists for equality (s == s[::-1]) takes O(N) time complexity because it requires comparing each element in the list."""

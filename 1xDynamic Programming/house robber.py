2c5f87b39ef28bfd40b6b184559b26c2ae0fe4ff
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2024-01-03 20:24:33 -0500

    House Robbery

---
+++ 1xDynamic Programming/house robber.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+
+
+algorithm = Solution()
+nums = [2,7,9,3,1]
+print(algorithm.rob(nums))

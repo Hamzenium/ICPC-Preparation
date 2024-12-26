09beaa2d2bf82eb5e176de6067aaac47cc8c2c11
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2024-12-26 13:58:10 -0500

    Bruteforce TwoSum

---
+++ arrays & hashing/twosum.py
@@ -0,0 +1,14 @@
+
+
+
+class bruteforce(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j and nums[i] + nums[j] == target:
+                    return [i,j]
+    
+nums = [2,7,11,15]
+target = 9
+solution = Solution()
+print(solution.twoSum(nums,target))

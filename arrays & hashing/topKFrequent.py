b2c97d04af81c5071dc1a6d1472d6dd0b9af8bc7
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-07 05:29:37 -0500

    Added Brute Force Top K Frequent

--- arrays & hashing/topKFrequent.py
+++ arrays & hashing/topKFrequent.py
@@ -14,6 +14,23 @@ class Solution(object):
                 res.append(n)
                 if len(res) == k:
                     return res
+class bruteforce(object):
+    @staticmethod
+    def topKFrequent(nums, k):
+        # Count frequency of each element
+        freq_map = {}
+        for num in nums:
+            freq_map[num] = freq_map.get(num, 0) + 1
+        
+        # Sort elements by frequency in descending order using lambda
+        sorted_nums = sorted(freq_map.items(), key=lambda x: x[1], reverse=True)
+        
+        # Extract the top k frequent elements
+        res = []
+        for i in range(k):
+            res.append(sorted_nums[i][0])
+        
+        return res
 
 nums = [1, 1, 1, 2, 2, 3]
 k = 2

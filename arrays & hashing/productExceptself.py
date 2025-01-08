a2bbf4637ae4552a1d5071fb7f2be6c2d468d57f
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-08 05:40:41 -0500

    Optimzed: ProductExceptSelf

--- arrays & hashing/productExceptself.py
+++ arrays & hashing/productExceptself.py
@@ -1,4 +1,16 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        res = [1] * (len(nums))
 
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
 
         
 class BruteForce(object):
@@ -17,10 +29,6 @@ class BruteForce(object):
 
 
 
-
-
-
-
 nums = [1,2,3,4]
 sol = Solution()
-sol.productExceptSelf(nums)
+print(sol.productExceptSelf(nums))

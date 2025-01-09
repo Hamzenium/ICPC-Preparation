5e7aabb424fc46317165543f6b6cf971f448fd17
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-08 22:11:34 -0500

    Optimzed: ContainsDuplicateII

--- containsNearbyDuplicate.py
+++ containsNearbyDuplicate.py
@@ -1,3 +1,16 @@
+from collections import defaultdict
+
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        num_dict = defaultdict(int)
+        for i in range(len(nums)):
+            if nums[i] in num_dict and abs(i - num_dict[nums[i]]) <= k:
+                return True
+            num_dict[nums[i]] = i
+        return False
+
+
 class BruteForce(object):
     def containsNearbyDuplicate(self, nums, k):
         for i in range(len(nums)):

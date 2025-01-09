f986fedee1bb16b957049bcf6cb8ba8dbd09bd6e
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-08 19:40:20 -0500

    Optimized: Longest Consecutive

--- arrays & hashing/LongestConsecutiveSequence.py
+++ arrays & hashing/LongestConsecutiveSequence.py
@@ -1,16 +1,5 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        # sortedList = sorted(nums)
-        # counter = 1
-        # largest = 1
-        # for i in range(len(nums)-2):
-        #     if sortedList[i + 1]  - sortedList[i]  == 1:
-        #         counter =  counter + 1
-        #         if counter >  largest :
-        #             largest = counter
-        #     else:
-        #         counter = 0
-        # return largest
         res = 0
         store = set(nums)
 
@@ -22,7 +11,17 @@ class Solution(object):
             res = max(res, streak)
         return res
 
-
+class Solution:
+    def longestConsecutive(self, nums):
+        numSet = set(nums)
+        longest = 0
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
 
 nums = [100,4,200,1,3,2]
 sol = Solution()

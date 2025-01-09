521b2127ab6cf7cd6af0272174af18822e749ffe
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-08 21:28:43 -0500

    Brute Force: isValidSudoku

--- isvalidSudoko.py
+++ isvalidSudoko.py
@@ -19,6 +19,39 @@ class Solution(object):
                     squares[r // 3, c // 3].add(board[r][c])
         return True
     
+
+class Solution:
+    def isValidSudoku(self, board):
+        for row in range(9):
+            seen = set()
+            for i in range(9):
+                if board[row][i] == ".": 
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+        
+        for col in range(9):
+            seen = set()
+            for i in range(9):
+                if board[i][col] == ".":
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+            
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square//3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        return True
 board = [["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
 ,[".","9","8",".",".",".",".","6","."]

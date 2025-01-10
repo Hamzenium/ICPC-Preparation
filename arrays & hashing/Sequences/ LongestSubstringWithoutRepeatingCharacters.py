fe8282d2e35a3da66ed09c8b7b6e82e9e11973f0
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-09 19:01:33 -0500

    Added Subfolders

---
+++ arrays & hashing/Sequences/ LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+
+
+
+s = "abcabcbb"
+solution = Solution()
+print(solution.lengthOfLongestSubstring(s))

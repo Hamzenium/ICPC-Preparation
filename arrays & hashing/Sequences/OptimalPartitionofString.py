6b564e96ad39ff897e09ed3056d3782bb431ecb4
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-09 21:37:30 -0500

    BruteForce: PartitionString

---
+++ arrays & hashing/Sequences/OptimalPartitionofString.py
@@ -0,0 +1,53 @@
+class Solution(object):
+   def partitionString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        seen = set()
+        counter = 0
+        while i < len(s):
+            j = i
+            while j < len(s):
+                if s[j] not in seen:
+                    seen.add(s[j])
+                    j = j + 1
+                else:
+                    counter = counter + 1
+                    seen.clear()
+            i = j
+            i = i + 1
+        if len(seen) > 0:
+            counter = counter + 1
+        return counter
+        
+
+
+class BruteForce(object):
+   def partitionString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        i = 0
+        seen = set()
+        counter = 0
+        while i < len(s):
+            j = i
+            while j < len(s):
+                if s[j] not in seen:
+                    seen.add(s[j])
+                    j = j + 1
+                else:
+                    counter = counter + 1
+                    seen.clear()
+            i = j
+            i = i + 1
+        if len(seen) > 0:
+            counter = counter + 1
+        return counter
+
+s = "cuieokbs"
+sol = Solution()
+print(sol.partitionString(s))

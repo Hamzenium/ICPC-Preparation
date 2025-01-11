8991c904f2b0f7f35f5df2955170825948517307
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2025-01-10 22:11:52 -0500

    Added Optimzed twoSum II

---
+++ TwoSum II.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
+
+
+class BruteForce(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(numbers)):
+            for j in range(len(numbers)):
+                if numbers[i] + numbers[j] == target and i != j:
+                    return [i+1,j+1]
